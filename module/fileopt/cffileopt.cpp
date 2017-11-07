95a18fec4892a6cc8cce2f89c3f7e5fce78ee752
AlfredYang1986 <yangyuanpig@gmail.com>
2017-11-07 21:57:52 +0800

    File Opration

---
+++ module/fileopt/cffileopt.cpp
@@ -0,0 +1,26 @@
+#include "cffileopt.h"
+#include <QList>
+
+CFFileOpt::CFFileOpt() {
+
+}
+
+CFFileOpt::~CFFileOpt() {
+
+}
+
+void CFFileOpt::saveToPath(const QString &path) {
+
+}
+
+void* CFFileOpt::loadFromPath(const QString &path) {
+    return NULL;
+}
+
+const QList<QString> CFFileOpt::resentPathLst() const {
+    return QList<QString>();
+}
+
+const QString CFFileOpt::getResentFilePath() const {
+    return "";
+}
---
+++ module/fileopt/cffileopt.h
@@ -0,0 +1,32 @@
+#ifndef CFFILEOPT_H
+#define CFFILEOPT_H
+
+#include "../basemodules/cfbasemodule.h"
+#include "../../module/modulemanagement/cfmm.h"
+
+/**
+ * @brief The CFFileOpt class
+ * 		save load and resent list
+ */
+
+class CFModuleManagement;
+
+class CFFileOpt : public CFBaseModule {
+
+    Q_OBJECT
+protected:
+    explicit CFFileOpt();
+
+public:
+    ~CFFileOpt();
+
+public:
+    void saveToPath(const QString& path);
+    void* loadFromPath(const QString& path);
+    const QList<QString> resentPathLst() const;
+    const QString getResentFilePath() const;
+
+    friend class CFModuleManagement;
+};
+
+#endif // CFFILEOPT_H
